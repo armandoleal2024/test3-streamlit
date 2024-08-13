09dd5f3cfa2ae63c79a97d8c2e1696bccd107874
armandoleal2024 <armandolealm@gmail.com>
2024-08-12 19:11:09 -0600

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -3,8 +3,5 @@ import streamlit as st
 st.title("Open AI ")
 st.sidebar.title("AI Apps")
 
-# Set the page config to wide mode
-st.set_page_config(layout="wide")
-
 applications = ["Blog Generation", "Generate Image", "Movie Recommendation"]
 application_choice = st.sidebar.radio("Choose an Application", applications)
