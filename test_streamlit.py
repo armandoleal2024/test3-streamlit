33bee66f10b86363e4886b0a4a439a63975f4f19
armandoleal2024 <armandolealm@gmail.com>
2024-08-12 19:31:54 -0600

    Update test_streamlit.py
    Se agregó opción "Others"

--- test_streamlit.py
+++ test_streamlit.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("Open AI ")
 st.sidebar.title("AI Apps")
 
-applications = ["Blog Generation", "Generate Image", "Movie Recommendation"]
+applications = ["Blog Generation", "Generate Image", "Movie Recommendation","Other"]
 application_choice = st.sidebar.radio("Choose an Application", applications)
